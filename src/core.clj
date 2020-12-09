fc5590091525bca2666759a55121127ae2e6113d
Emma <eagriffin@usf.edu>
2020-12-08 20:33:34 -0500

    Add forward declaration for certain functions

--- src/core.clj
+++ src/core.clj
@@ -1,9 +1,18 @@
 (ns core.clj)
 
+(declare double-point add-point)
+
 (defn get-inverse [x p]
   (inc (.indexOf
         (mapv #(mod (* x %) p)
               (range 1 p)) 1)))
+
+(defn calc-s [f [x1 y1] [x2 y2] a p]
+  (mod
+   (if (= f double-point)
+     (* (+ (* 3 x1 x1) a) (get-inverse (* 2 y2) p))
+     (* (- y2 y1) (get-inverse (- x2 x1) p)))
+   p))
 
 (defn double-and-add [k P curve]
   ;; curve = (y^2 = x^3 + 4x + 20 mod 29) => [1 4 20 29]
@@ -33,13 +42,6 @@
                                       s
                                       p]))
                      (if (= f1 double-point) (* 2 cur-k) (inc cur-k)))))))))
-
-(defn calc-s [f [x1 y1] [x2 y2] a p]
-  (mod
-   (if (= f double-point)
-     (* (+ (* 3 x1 x1) a) (get-inverse (* 2 y2) p))
-     (* (- y2 y1) (get-inverse (- x2 x1) p)))
-   p))
 
 (defn add-point [[x1 y1] [x2 y2] s p]
   (let [x3 (mod
